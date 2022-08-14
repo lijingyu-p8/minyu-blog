7a3bd4e916d6b41645ea53ba935fc0386e84a533
lijingyu-p8 <lijingyu_dlmu@163.com>
2022-08-14 12:59:05 +0800

    es集群分布式

--- docs/.vuepress/sidebar.ts
+++ docs/.vuepress/sidebar.ts
@@ -5,33 +5,33 @@ export default sidebar([
   "/planet",
   // "/slide",
   {
-    icon: "note",
+    icon: "tab",
     link: "/article",
     text: "所有文章"
   },
   {
     text: "RocketMQ",
-    icon: "note",
+    icon: "blog",
     prefix: "/RocketMQ/",
     link: "/RocketMQ/",
     children: [
       {
         text: "01.基础知识",
-        icon: "note",
+        icon: "article",
         collapsable: true,
         prefix: "article/",
         link: "01.基础知识"
       },
       {
         text: "02.集群搭建",
-        icon: "note",
+        icon: "article",
         collapsable: true,
         prefix: "article/",
         link: "02.集群搭建"
       },
       {
         text: "03.Java-API",
-        icon: "note",
+        icon: "article",
         collapsable: true,
         prefix: "article/",
         link: "03.Java-API"
@@ -58,58 +58,72 @@ export default sidebar([
   },
   {
     text: "ElasticSearch",
-    icon: "note",
+    icon: "blog",
     prefix: "/ElasticSearch/",
     link: "/ElasticSearch/",
     children: [
       {
         text: "01.安装",
-        icon: "note",
+        icon: "article",
         collapsable: true,
         prefix: "article/",
         link: "01.安装"
       },
       {
         text: "02.核心概念",
-        icon: "note",
+        icon: "article",
         collapsable: true,
         prefix: "article/",
         link: "02.核心概念"
       },
       {
         text: "03.索引",
-        icon: "note",
+        icon: "article",
         collapsable: true,
         prefix: "article/",
         link: "03.索引"
       },
       {
         text: "04.数据新增",
-        icon: "note",
+        icon: "article",
         collapsable: true,
         prefix: "article/",
         link: "04.数据新增"
       },
       {
         text: "05.数据查询",
-        icon: "note",
+        icon: "article",
         collapsable: true,
         prefix: "article/",
         link: "05.数据查询"
       },
       {
         text: "06.数据修改",
-        icon: "note",
+        icon: "article",
         collapsable: true,
         prefix: "article/",
         link: "06.数据修改"
       },
       {
         text: "07.数据删除",
-        icon: "note",
+        icon: "article",
         collapsable: true,
         prefix: "article/",
         link: "07.数据删除"
+      },
+      {
+        text: "10.集群分布式",
+        icon: "article",
+        collapsable: true,
+        prefix: "article/",
+        link: "10.集群分布式"
+      },
+      {
+        text: "11.ES相关原理",
+        icon: "article",
+        collapsable: true,
+        prefix: "article/",
+        link: "11.ES相关原理"
       },
     ],
   },
